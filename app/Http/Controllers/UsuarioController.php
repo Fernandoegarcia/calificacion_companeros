a149c53242351bb5fbfed6fc3a92999af677a3e3
FERNANDO GARCIA <fernandoegarcia25@gmail.com>
2020-11-13 11:54:23 -0400

    validaciones

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -7,6 +7,7 @@ use App\Models\Usuario;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
+use Illuminate\Validation\Rule;
 
 
 class UsuarioController extends Controller
@@ -20,6 +21,8 @@ class UsuarioController extends Controller
             'user_name'=>['required','unique:App\Models\Usuario,user_name'],
             'email' => ['required','email','unique:App\Models\Usuario,email'],
             'password' => ['required','string','min:8'],
+            'edad'=>['required', 'min:2'],
+            'tipo'=>['required', Rule::in(['empleador','empleado','desempleado'])]
 
         ]);
 
@@ -28,7 +31,7 @@ class UsuarioController extends Controller
         $usuario = new Usuario();
 
         $usuario->fill(
-            $request->merge(['tipo'=>'empleado'])->only('nombre', 'cargo', 'tipo', 'fecha_nacimiento','compañia','sexo', 'edad', 'password', 'user_name', 'email')
+            $request->only('nombre', 'cargo', 'tipo', 'fecha_nacimiento','compañia','sexo', 'edad', 'password', 'user_name', 'email')
         );
 
         $usuario->save();
@@ -50,7 +53,9 @@ class UsuarioController extends Controller
             'email' => ['required','email','unique:App\Models\Usuario,email'],
             'password' => ['required','string','min:8'],
             'aptitudes'=>['required', 'array', 'min:1'],
-            'aptitudes.*'=>['numeric', 'exists:aptitudes,id']
+            'aptitudes.*'=>['numeric', 'exists:aptitudes,id'],
+            'tipo'=>['required', Rule::in(['empleador','empleado','desempleado'])],
+            'edad'=>['required', 'min:2']
 
         ]);
 
